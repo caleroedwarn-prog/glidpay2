1ad720a4377445329b3b33e314fdd810eb2b3114
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-05-11 15:28:18 +0100

    A dashboard which users can access after successful Login

--- glidpay-frontend/src/App.tsx
+++ glidpay-frontend/src/App.tsx
@@ -1,1 +1,4 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import { AuthProvider } from './context/AuthContext';
