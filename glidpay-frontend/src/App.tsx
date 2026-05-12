4e4409f61cddc94d3e5cc0be4aa3a5e2902f970c
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-05-12 15:37:37 +0100

    Push: included a home page and its stylling for smooth UX experience by glidpay users

--- glidpay-frontend/src/App.tsx
+++ glidpay-frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import LoginPage from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import RegisterPage from './pages/Registers';
+import HomePage from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/home' element={<HomePage/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<RegisterPage />} />
