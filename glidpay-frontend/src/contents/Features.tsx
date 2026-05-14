36dbec71390648771249485e710ca3c5bf2785d1
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-05-14 12:22:38 +0100

    making cards content for the home page: Features of GlidPay

--- glidpay-frontend/src/contents/Features.tsx
+++ glidpay-frontend/src/contents/Features.tsx
@@ -1,3 +1,36 @@
-function Features(){
-    
+export default function Features(){
+   return(
+    <>
+    <div className="Features-header">
+        <h1>Features</h1>
+        <pre>Powerful Financial FeatturesBuilt for the Modern World
+            <p> GlidPay gives you a fast, secure, and simple way<br />to send, receive, store, and manage money globally <br /> -anytime and anywhere</p>
+            <span>-GEt STARTED|Download App</span>
+        </pre>
+    </div>
+
+    <div className="features">
+        <div className="feature">
+            <h2>Global Feature</h2>
+            <p>Send and receive money instantly across countries with fast processing and real-time transaction update</p>
+            <pre>Real-time transfer</pre>
+            <pre>Fast confirmation</pre>
+            <pre>24/7 transaction</pre>
+        </div>
+
+        <div className="feature">
+            <h2>Multi-Currency Wallet</h2>
+            <p>Store and manage multple currencies in one secure wallet without needing multiple bank accounts</p>
+            <pre>Multiple currencies</pre>
+            <pre>Easy conversion</pre>
+            <pre>Global accessibility</pre>
+        </div>
+
+        <div className="feature">
+            <h2>Advanced Security</h2>
+            <
+            </div>
+    </div>
+    </>
+   ) 
 }
