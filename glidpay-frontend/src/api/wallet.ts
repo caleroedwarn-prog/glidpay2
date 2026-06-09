d8661e2b3ebd0d65b8d1b4cf8bf97e8397c57955
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-06-09 15:55:38 +0100

    Enhance wallet API integration and update Dashboard component
    - Added deposit, withdraw, transfer, and getTransaction functions to wallet API.
    - Implemented balance fetching in Dashboard using useEffect.
    - Improved Dashboard layout and styling for better user experience.
    - Updated CSS for Dashboard and Navbar for enhanced visuals.

--- glidpay-frontend/src/api/wallet.ts
+++ glidpay-frontend/src/api/wallet.ts
@@ -1,4 +1,10 @@
-import { type WalletBalance } from "../types/wallet";
+import { type WalletBalance,
+         type DepositeRequest,
+         type WithdrawRequest,
+         type TransferRequest,
+         type WalletResponse,
+         type TransactionList
+ } from "../types/wallet";
 
 const BASE_URL = "https://localhost:8000";
 const getToken = localStorage.getItem("token");
@@ -7,11 +13,52 @@ const authHeaders = () => ({
     Authorization: `Bearer ${getToken}`
 })
 
+
 export const getBalance = async (): Promise<WalletBalance> => {
     const response = await fetch(`${BASE_URL}/wallet/balance`, {
         method: "GET",
         headers: authHeaders()
     })
     if(!response.ok) throw new Error("Failed to fetch balance")
+        return response.json()
+}
+
+export const deposit = async(body: DepositeRequest): Promise<WalletResponse> => {
+    const response = await fetch(`${BASE_URL}/wallet/deposit`, {
+        method: "POST",
+        headers: authHeaders(),
+        body: JSON.stringify(body)
+
+    })
+    if(!response.ok) throw new Error("Failed to place Deposite try again later")
+        return response.json()
+}
+
+export const withdraw = async(body: WithdrawRequest): Promise<WalletResponse> => {
+    const response = await fetch(`${BASE_URL}/wallet/withdraw`, {
+        method: "POST",
+        headers: authHeaders(),
+        body: JSON.stringify(body)
+    })
+    if(!response.ok) throw new Error("Failed to place Withdrawal, Try again later")
+        return response.json()
+}
+
+export const transfer = async(body: TransferRequest): Promise<WalletResponse> => {
+    const response = await fetch(`${BASE_URL}/wallet/transfer`, {
+        method: "POST",
+        headers: authHeaders(),
+        body: JSON.stringify(body)
+    })
+    if(!response.ok) throw new Error("Failed to transfer, try again later")
+        return response.json()
+}
+
+export const getTransaction = async(): Promise<TransactionList> => {
+    const response = await fetch(`${BASE_URL}/wallet/transactions`, {
+        method: "GET",
+        headers: authHeaders()
+    })
+    if(!response.ok) throw new Error("Failed to fetch transactions")
         return response.json()
 }
--- glidpay-frontend/src/pages/Dashboard.tsx
+++ glidpay-frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../style/Dashboard.css";
 import { IoNotifications } from "react-icons/io5";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { getBalance } from "../api/wallet";
+import { type WalletBalance } from "../types/wallet";
 function DashboardPage(){
-    const [isShow, setIsShow] = useState(false);
+    const [showBalance, setShowBalance] = useState(false);
+    const [ balance, setBalance ] = useState< number | null >(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState("")
     const toggleBalance = () => {
-        setIsShow(!isShow)
+        setShowBalance(!showBalance)
     }
+
+    const exchangeRate = {
+        XAF_TO_USD: 0.0016,
+        XAF_TO_NGN: 2.48,
+        USD_TO_XAF: 620,
+        USD_TO_NGN: 1550,
+        NGN_TO_XAF: 0.40,
+        NGN_TO_USD: 0.00064
+    }
+
+    useEffect(() => {
+        const fetchBalance = async() => {
+            setIsLoading(true)
+            try{
+                const data: WalletBalance = await getBalance()
+                setBalance(data.balance)
+            } catch{
+                setError("Failed to load balance")
+            }finally{
+                setIsLoading(false)
+            }
+        }
+        fetchBalance()
+    }, [])
     return(
         <>
         <div className="borderBox">
@@ -26,16 +55,49 @@ function DashboardPage(){
                     </div>
                 </div>
 
+                 <div className="header">
+                     <h2>Total Balance:</h2>
+                 </div>
+
                 <div className="Balance">
-                    <h2>Total Balance</h2>
-                    <p>Balance:
-                       {isShow? "2038" : "*********"}
-                    </p>
+                    <pre>
+                       {
+                        isLoading ? "Loading..." : showBalance ? `$${balance}`:"*******" 
+                       }
+                       
+                    </pre>
                     <button className="toggle-btn" onClick={toggleBalance}>
-                        { isShow ? <AiOutlineEye size={22} color="white" /> : <AiOutlineEyeInvisible size={22} color="white" /> }
+                        { showBalance ? <AiOutlineEye size={22}  color="white" /> : <AiOutlineEyeInvisible size={22} color="white" /> }
                     </button>
                 </div>
+                <div className="error">
+                 {error && <p>{error}</p>}
+                 </div><br />
+
+                 <div className="transaction_box">
+                    <button type="submit"  className="content_type">
+                        <p>Deposit</p>
+                    </button>
+
+                     <button type="submit" className="content_type">
+                        <p>Transfer</p>
+                    </button>   
+
+                     <button type="submit" className="content_type">
+                        <p>Withdraw</p>
+                    </button>
+
+                     <button type="submit" className="content_type1">
+                        <pre>History</pre>
+                    </button>
+                 </div>
             </div>
+
+               <div className="buttom_bar">
+                    <div className="content">
+                        
+                    </div>
+                 </div>     
         </div>
         </>
     )
